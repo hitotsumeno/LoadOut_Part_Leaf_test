dfe89b3a02e8cb4c6584823219e767f4eac15a1f
Oleksandr <152290002+hitotsumeno@users.noreply.github.com>
2026-07-22 17:18:57 +0200

    Setup a new Jump vars

--- Assets/_Scripts/Player/StateMachine/ConcreteStates/PlayerJumpState.cs
+++ Assets/_Scripts/Player/StateMachine/ConcreteStates/PlayerJumpState.cs
@@ -7,10 +7,42 @@ public class PlayerJumpState : PlayerBaseState
 {
     [SerializeField] private float VerticalForce = 9f;
 
+    // Jump Event var
+    bool JumpWasPressed;
+    bool JumpWasRelease;
+    bool JumpIsHeld;
+
+    // Jump var
+    public float VerticalVelocity {get; private set; }
+    private bool _isJumping;
+    private bool _isFastFalling;
+    private bool _isFalling;
+    private bool _fastFallTime;
+    private bool _fastFallRealeaseSpeed;
+    private int _numberOfJumpUsed;
+
+    // Apex var
+    private float _apexPoint;
+    private float _timePastApexThreshold;
+    private bool _isPastApexTreshold;
+
+    // Jump buffer var
+    private float _jumpBufferTimer;
+    private bool _jumpReleaseDuringBuffer;
+
+    // Coyote Time var
+
+    private float _coyoteTimer;
+
+
+
+
     public override void EnterState()
     {
         Reader.MoveEvent += HandleMove;
         Reader.JumpPressedEvent += HandleJump;
+        // Reader.JumpIsHeldEvent +=
+        // Reader.JumpReleaseEvent +=
         Jump();
         Debug.Log("Player enters Jump state");
     }
